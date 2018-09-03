f127035038a31d059333be06bc95c9ddd5eed42e
Elvey <matt.elvey@gmail.com>
2018-09-04 00:50:32 +0930

    scroll between days

--- src/js/draw-days.js
+++ src/js/draw-days.js
@@ -5,10 +5,14 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 let templateString = undefined;
 let template = undefined;
 let target = undefined;
+let data = undefined;
+
+const setData = (dayData) => data = dayData;
+const getData = () => data;
 
 const getTarget = () => {
 	if(!target) {
-		target = document.querySelector('main');
+		target = document.querySelector('.day__container');
 	}
 	return target;
 };
@@ -19,10 +23,13 @@ const renderDay = (data) => {
 		template = ejs.compile(templateString);
 	}
 
-	return template(data);
+	const html = template(data);
+	const templateElem = document.createElement('template');
+	templateElem.innerHTML = html.trim();
+	return templateElem.content.firstChild;
 };
 
-function drawDay(day, vendors) {
+function drawDay(day, vendors, classes) {
 	var open = [];
 
 	vendors.forEach((vendor) => {
@@ -40,7 +47,7 @@ function drawDay(day, vendors) {
 				{
 					openLocation,
 					openDay: {
-						day: openDay.day,
+						day,
 						start: timeConvert(openDay.start),
 						end: timeConvert(openDay.end)
 					}
@@ -56,18 +63,77 @@ function drawDay(day, vendors) {
 		vendors: open
 	});
 
-	getTarget().innerHTML += content;
+	const next = content.querySelector('.day__next-btn');
+	next.addEventListener('click', nextDay);
+	const prev = content.querySelector('.day__prev-btn');
+	prev.addEventListener('click', prevDay);
+
+	if(classes) {
+		const classArr = Array.isArray(classes) ? classes : [classes];
+		classArr.forEach((className) => {
+			content.classList.add(className);
+		});
+	}
+
+	return content;
 }
 
 function drawDays(dayData) {
+	setData(dayData);
 	getTarget().innerHTML = null;
 
 	var now = new Date();
 	var today = now.getDay();
+	var yesterday = today > 0 ? today - 1 : 6;
+	var tomorrow = today < 6 ? today + 1 : 0;
 
-	drawDay(today, dayData);
+	getTarget().appendChild(
+		drawDay(yesterday, dayData)
+	);
+	getTarget().appendChild(
+		drawDay(today, dayData)
+	)
+	getTarget().appendChild(
+		drawDay(tomorrow, dayData)
+	);
 
 
+}
+
+function nextDay() {
+	const target = getTarget();
+	const days = target.childNodes;
+	const lastDay = days[days.length - 1];
+	const dayIndex = parseInt(lastDay.dataset.day);
+	const nextDay = dayIndex < 6 ? dayIndex + 1 : 0;
+	const day = drawDay(nextDay, getData());
+	const listen = (evt) => {
+		target.classList.remove('day--next');
+		target.removeEventListener('transitionend', listen);
+		target.removeChild(days[0]);
+		target.appendChild(day);
+	};
+	
+	target.addEventListener('transitionend', listen);
+	target.classList.add('day--next');
+}
+
+function prevDay() {
+	const target = getTarget();
+	const days = target.childNodes;
+	const firstDay = days[0];
+	const dayIndex = parseInt(firstDay.dataset.day);
+	const nextDay = dayIndex > 0 ? dayIndex - 1 : 6;
+	const day = drawDay(nextDay, getData());
+	const listen = () => {
+		target.classList.remove('day--previous');
+		target.removeEventListener('transitionend', listen);
+		target.removeChild(days[days.length - 1]);
+		target.prepend(day);
+	};
+	
+	target.addEventListener('transitionend', listen);
+	target.classList.add('day--previous');
 }
 
 export default drawDays;
